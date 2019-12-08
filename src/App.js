67775fa51713aa31d763d6e972b7e3e513261b70
ullataponen <ulla.taponen@gmail.com>
2019-12-08 13:18:11 +0100

    Initial upload of all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Carlist from "./components/Carlist";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="h6">
+						<span role="img" aria-label="car" aria-hidden="true">
+							🚗
+						</span>
+						Ulla's Second Hand Car Shop
+						<span role="img" aria-label="car" aria-hidden="true">
+							🚗
+						</span>
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Carlist />
+		</div>
+	);
 }
 
 export default App;
